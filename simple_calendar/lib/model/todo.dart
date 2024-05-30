566eb1811a6b2d6dfa58efa3132bc98b3d3f2794
WonHyun <coy4728@naver.com>
2024-05-30 13:21:29 +0900

    fix: Todo to immutable

--- simple_calendar/lib/model/todo.dart
+++ simple_calendar/lib/model/todo.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:simple_calendar/global/colors.dart';
+import 'package:simple_calendar/utils/generator_util.dart';
 
 class Todo {
-  String title;
-  String description;
-  DateTime startTime;
-  DateTime endTime;
-  List<String> attendees;
-  Color labelColor;
+  final String id;
+  final String title;
+  final String description;
+  final DateTime startTime;
+  final DateTime endTime;
+  final List<String> attendees;
+  final Color labelColor;
 
   Todo({
-    required this.title,
-    required this.description,
-    required this.startTime,
-    required this.endTime,
-    required this.attendees,
-    required this.labelColor,
-  });
+    String? title,
+    String? description,
+    DateTime? startTime,
+    DateTime? endTime,
+    List<String>? attendees,
+    Color? labelColor,
+  })  : id = uuid.v4(),
+        title = title ?? "",
+        description = description ?? "",
+        startTime = startTime ?? DateTime.now(),
+        endTime = endTime ?? DateTime.now(),
+        attendees = attendees ?? const [],
+        labelColor = labelColor ?? LabelColors.lemon;
+
+  Todo copyWith({
+    String? title,
+    String? description,
+    DateTime? startTime,
+    DateTime? endTime,
+    List<String>? attendees,
+    Color? labelColor,
+  }) {
+    return Todo(
+      title: title ?? this.title,
+      description: description ?? this.description,
+      startTime: startTime ?? this.startTime,
+      endTime: endTime ?? this.endTime,
+      attendees: attendees ?? this.attendees,
+      labelColor: labelColor ?? this.labelColor,
+    );
+  }
 }
