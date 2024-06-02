69f25977409a4e6b74052b9619aa63bf3f46b74e
WonHyun <coy4728@naver.com>
2024-06-02 22:08:30 +0900

    feat: add some gradient

--- movieflix/lib/screens/home_screen.dart
+++ movieflix/lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:movieflix/global/colors.dart';
 import 'package:movieflix/global/enum.dart';
+import 'package:movieflix/global/theme.dart';
 import 'package:movieflix/providers/notifiers/movie_list_notifier.dart';
 import 'package:movieflix/providers/providers.dart';
 import 'package:movieflix/providers/states/movie_list_state.dart';
@@ -56,6 +57,7 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final themeState = ref.watch(themeProvider);
     return GestureDetector(
       onTap: _onScaffoldTap,
       child: Scaffold(
@@ -79,81 +81,110 @@ class HomeScreen extends ConsumerWidget {
             ),
           ),
         ),
-        body: RefreshIndicator(
-          onRefresh: () async => _onRefresh(context, ref),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 20,
-                right: 20,
-                bottom: 50,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    child: TabTitle(title: "Popular Movies"),
+        body: Container(
+          decoration: themeState.theme == MovieflixTheme.dark
+              ? BoxDecoration(
+                  gradient: RadialGradient(
+                    center: Alignment.bottomCenter,
+                    radius: 0.8,
+                    stops: const [0.2, 0.5, 0.75, 1.0],
+                    colors: [
+                      Colors.transparent,
+                      Colors.purple.shade400,
+                      Colors.pink.shade400,
+                      Colors.transparent,
+                    ],
                   ),
-                  MovieListView(
-                    width: 300,
-                    height: 200,
-                    isRanked: true,
-                    moviesState:
-                        _getMoviesState(context, ref, MovieListType.popular),
-                    moviesNotifier:
-                        _getMoviesNotifier(context, ref, MovieListType.popular),
+                )
+              : BoxDecoration(
+                  gradient: RadialGradient(
+                    center: Alignment.bottomCenter,
+                    radius: 0.8,
+                    stops: const [0.2, 0.5, 0.75, 1.0],
+                    colors: [
+                      Colors.white,
+                      Colors.purple.shade100,
+                      Colors.pink.shade100,
+                      Colors.white,
+                    ],
                   ),
-                  Offstage(
-                    offstage: _getMoviesState(
-                            context, ref, MovieListType.recentlyViewed)
-                        .movies
-                        .isEmpty,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 20),
-                          child: TabTitle(title: "Recently Viewed"),
-                        ),
-                        MovieListView(
-                          width: 150,
-                          height: 150,
-                          isShowTitle: false,
-                          moviesState: _getMoviesState(
-                              context, ref, MovieListType.recentlyViewed),
-                          moviesNotifier: _getMoviesNotifier(
-                              context, ref, MovieListType.recentlyViewed),
-                        ),
-                      ],
+                ),
+          child: RefreshIndicator(
+            onRefresh: () async => _onRefresh(context, ref),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  bottom: 50,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: TabTitle(title: "Popular Movies"),
                     ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    child: TabTitle(title: "Now in Cinemas"),
-                  ),
-                  MovieListView(
-                    width: 150,
-                    height: 150,
-                    moviesState:
-                        _getMoviesState(context, ref, MovieListType.nowPlaying),
-                    moviesNotifier: _getMoviesNotifier(
-                        context, ref, MovieListType.nowPlaying),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    child: TabTitle(title: "Coming soon"),
-                  ),
-                  MovieListView(
-                    width: 150,
-                    height: 150,
-                    isShowDDay: true,
-                    moviesState:
-                        _getMoviesState(context, ref, MovieListType.comingSoon),
-                    moviesNotifier: _getMoviesNotifier(
-                        context, ref, MovieListType.comingSoon),
-                  ),
-                ],
+                    MovieListView(
+                      width: 300,
+                      height: 200,
+                      isRanked: true,
+                      moviesState:
+                          _getMoviesState(context, ref, MovieListType.popular),
+                      moviesNotifier: _getMoviesNotifier(
+                          context, ref, MovieListType.popular),
+                    ),
+                    Offstage(
+                      offstage: _getMoviesState(
+                              context, ref, MovieListType.recentlyViewed)
+                          .movies
+                          .isEmpty,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            child: TabTitle(title: "Recently Viewed"),
+                          ),
+                          MovieListView(
+                            width: 150,
+                            height: 150,
+                            isShowTitle: false,
+                            moviesState: _getMoviesState(
+                                context, ref, MovieListType.recentlyViewed),
+                            moviesNotifier: _getMoviesNotifier(
+                                context, ref, MovieListType.recentlyViewed),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: TabTitle(title: "Now in Cinemas"),
+                    ),
+                    MovieListView(
+                      width: 150,
+                      height: 150,
+                      moviesState: _getMoviesState(
+                          context, ref, MovieListType.nowPlaying),
+                      moviesNotifier: _getMoviesNotifier(
+                          context, ref, MovieListType.nowPlaying),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: TabTitle(title: "Coming soon"),
+                    ),
+                    MovieListView(
+                      width: 150,
+                      height: 150,
+                      isShowDDay: true,
+                      moviesState: _getMoviesState(
+                          context, ref, MovieListType.comingSoon),
+                      moviesNotifier: _getMoviesNotifier(
+                          context, ref, MovieListType.comingSoon),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
