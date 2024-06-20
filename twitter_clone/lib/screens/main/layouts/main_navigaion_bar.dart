403e06b435fa643918e305ec5fc9d4c4708d362f
WonHyun <coy4728@naver.com>
2024-06-21 00:04:51 +0900

    ref: seperate main navigation bar to file

---
+++ twitter_clone/lib/screens/main/layouts/main_navigaion_bar.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:twitter_clone/global/enum.dart';
+import 'package:twitter_clone/global/extensions.dart';
+import 'package:twitter_clone/providers/providers.dart';
+import 'package:twitter_clone/screens/activity/activity_screen.dart';
+import 'package:twitter_clone/screens/home/home_screen.dart';
+import 'package:twitter_clone/screens/main/components/bottom_navigation_tab.dart';
+import 'package:twitter_clone/screens/posting/posting_modal.dart';
+import 'package:twitter_clone/screens/profile/profile_screen.dart';
+import 'package:twitter_clone/screens/search/search_screen.dart';
+
+class MainNavigaionBar extends StatefulWidget {
+  const MainNavigaionBar({super.key});
+
+  @override
+  State<MainNavigaionBar> createState() => _MainNavigaionBarState();
+}
+
+class _MainNavigaionBarState extends State<MainNavigaionBar> {
+  String _getRoutePath(MainScreenType type) {
+    switch (type) {
+      case MainScreenType.home:
+        return HomeScreen.routePath;
+      case MainScreenType.search:
+        return Searchscreen.routePath;
+      case MainScreenType.activity:
+        return ActivityScreen.routePath;
+      case MainScreenType.user:
+        return ProfileScreen.routePath;
+      case MainScreenType.posting:
+        return HomeScreen.routePath;
+    }
+  }
+
+  Future<void> _onChangeScreens(
+    BuildContext context,
+    WidgetRef ref,
+    MainScreenType type,
+  ) async {
+    final screenNotifier = ref.watch(mainScreenProvider.notifier);
+    if (type == MainScreenType.posting) {
+      await showModalBottomSheet(
+        isScrollControlled: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        context: context,
+        builder: (context) {
+          return const PostingModal();
+        },
+      ).then((value) {
+        if (mounted) {
+          ref.watch(postingProvider.notifier).resetPostingInfo();
+          screenNotifier.updateCurrentScreen(MainScreenType.home);
+          context.go(_getRoutePath(MainScreenType.home));
+        }
+      });
+    } else {
+      if (mounted) {
+        screenNotifier.updateCurrentScreen(type);
+        context.go(_getRoutePath(type));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, child) {
+      final screenState = ref.watch(mainScreenProvider);
+      return BottomAppBar(
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: MainScreenType.values
+              .map(
+                (type) => BottomNavigationTab(
+                  selectedIcon: type.selectedIcon,
+                  unselectedIcon: type.unselectedIcon,
+                  isSelected: screenState.currentScreen == type,
+                  onTap: () => _onChangeScreens(context, ref, type),
+                ),
+              )
+              .toList(),
+        ),
+      );
+    });
+  }
+}
--- twitter_clone/lib/screens/main/main_screen.dart
+++ twitter_clone/lib/screens/main/main_screen.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:twitter_clone/global/enum.dart';
-import 'package:twitter_clone/global/extensions.dart';
-import 'package:twitter_clone/providers/providers.dart';
-import 'package:twitter_clone/screens/activity/activity_screen.dart';
-import 'package:twitter_clone/screens/home/home_screen.dart';
-import 'package:twitter_clone/screens/main/widgets/bottom_navigation_tab.dart';
-import 'package:twitter_clone/screens/posting/posting_modal.dart';
-import 'package:twitter_clone/screens/profile/profile_screen.dart';
-import 'package:twitter_clone/screens/search/search_screen.dart';
+import 'package:twitter_clone/screens/main/layouts/main_navigaion_bar.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({
@@ -24,72 +14,6 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
       body: child,
       bottomNavigationBar: const MainNavigaionBar(),
-    );
-  }
-}
-
-class MainNavigaionBar extends ConsumerWidget {
-  const MainNavigaionBar({super.key});
-
-  String _getRoutePath(MainScreenType type) {
-    switch (type) {
-      case MainScreenType.home:
-        return HomeScreen.routePath;
-      case MainScreenType.search:
-        return Searchscreen.routePath;
-      case MainScreenType.activity:
-        return ActivityScreen.routePath;
-      case MainScreenType.user:
-        return ProfileScreen.routePath;
-      case MainScreenType.posting:
-        return HomeScreen.routePath;
-    }
-  }
-
-  Future<void> _onChangeScreens(
-    BuildContext context,
-    WidgetRef ref,
-    MainScreenType type,
-  ) async {
-    final screenNotifier = ref.watch(mainScreenProvider.notifier);
-    if (type == MainScreenType.posting) {
-      await showModalBottomSheet(
-        isScrollControlled: true,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        context: context,
-        builder: (context) {
-          return const PostingModal();
-        },
-      );
-      ref.watch(postingProvider.notifier).resetPostingInfo();
-      screenNotifier.updateCurrentScreen(MainScreenType.home);
-      context.go(_getRoutePath(MainScreenType.home));
-    } else {
-      screenNotifier.updateCurrentScreen(type);
-      context.go(_getRoutePath(type));
-    }
-  }
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final screenState = ref.watch(mainScreenProvider);
-    return BottomAppBar(
-      height: 70,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: MainScreenType.values
-            .map(
-              (type) => BottomNavigationTab(
-                selectedIcon: type.selectedIcon,
-                unselectedIcon: type.unselectedIcon,
-                isSelected: screenState.currentScreen == type,
-                onTap: () => _onChangeScreens(context, ref, type),
-              ),
-            )
-            .toList(),
-      ),
     );
   }
 }
