b24c33f74a1fd4c9dec5701aaaba2235c56cb27d
WonHyun Choi <coy4728@naver.com>
2024-07-04 01:17:21 +0900

    build: add "pull_to_refresh" library

--- twitter_clone/lib/screens/home/home_screen.dart
+++ twitter_clone/lib/screens/home/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:twitter_clone/providers/notifiers/post_notifier.dart';
 import 'package:twitter_clone/screens/home/layouts/post_view.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({
     super.key,
   });
@@ -13,38 +14,65 @@ class HomeScreen extends ConsumerWidget {
   static const routePath = "/home";
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  final RefreshController _controller =
+      RefreshController(initialRefresh: false);
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return ref.watch(postProvider).when(
           data: (post) {
             return Material(
-              child: CustomScrollView(
-                slivers: [
-                  SliverAppBar(
-                    centerTitle: true,
-                    surfaceTintColor: Colors.transparent,
-                    backgroundColor: Colors.transparent,
-                    title: FaIcon(
-                      FontAwesomeIcons.threads,
-                      color: Theme.of(context).colorScheme.inverseSurface,
-                      size: 36,
-                    ),
-                  ),
-                  SliverToBoxAdapter(
-                    child: ListView.separated(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      shrinkWrap: true,
-                      primary: false,
-                      itemCount: post.length,
-                      itemBuilder: (context, index) {
-                        return PostView(post: post[index]);
-                      },
-                      separatorBuilder: (context, index) => const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        child: Divider(),
+              child: SmartRefresher(
+                controller: _controller,
+                enablePullUp: true,
+                enablePullDown: true,
+                onRefresh: () async {
+                  await ref.read(postProvider.notifier).refresh();
+                  _controller.refreshCompleted();
+                },
+                onLoading: () async {
+                  await ref.read(postProvider.notifier).fetchNextPosts();
+                  _controller.loadComplete();
+                },
+                child: CustomScrollView(
+                  slivers: [
+                    SliverAppBar(
+                      centerTitle: true,
+                      surfaceTintColor: Colors.transparent,
+                      backgroundColor: Colors.transparent,
+                      title: FaIcon(
+                        FontAwesomeIcons.threads,
+                        color: Theme.of(context).colorScheme.inverseSurface,
+                        size: 36,
                       ),
                     ),
-                  ),
-                ],
+                    SliverToBoxAdapter(
+                      child: ListView.separated(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        shrinkWrap: true,
+                        primary: false,
+                        itemCount: post.length,
+                        itemBuilder: (context, index) {
+                          return PostView(post: post[index]);
+                        },
+                        separatorBuilder: (context, index) => const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          child: Divider(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             );
           },
