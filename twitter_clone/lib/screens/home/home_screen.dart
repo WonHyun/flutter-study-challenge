f8ce565b8ed427e2cf0990e68d0c969c903e9aa6
WonHyun <coy4728@naver.com>
2024-07-05 15:29:16 +0900

    feat: implement empty view if have no threads

--- twitter_clone/lib/screens/home/home_screen.dart
+++ twitter_clone/lib/screens/home/home_screen.dart
@@ -44,35 +44,57 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                   await ref.read(postProvider.notifier).fetchNextPosts();
                   _controller.loadComplete();
                 },
-                child: CustomScrollView(
-                  slivers: [
-                    SliverAppBar(
-                      centerTitle: true,
-                      surfaceTintColor: Colors.transparent,
-                      backgroundColor: Colors.transparent,
-                      title: FaIcon(
-                        FontAwesomeIcons.threads,
-                        color: Theme.of(context).colorScheme.inverseSurface,
-                        size: 36,
+                child: post.isEmpty
+                    ? const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.threads,
+                            size: 36,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "We have no threads today.\nlet's show your days.",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      )
+                    : CustomScrollView(
+                        slivers: [
+                          SliverAppBar(
+                            centerTitle: true,
+                            surfaceTintColor: Colors.transparent,
+                            backgroundColor: Colors.transparent,
+                            title: FaIcon(
+                              FontAwesomeIcons.threads,
+                              color:
+                                  Theme.of(context).colorScheme.inverseSurface,
+                              size: 36,
+                            ),
+                          ),
+                          SliverToBoxAdapter(
+                            child: ListView.separated(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              shrinkWrap: true,
+                              primary: false,
+                              itemCount: post.length,
+                              itemBuilder: (context, index) {
+                                return PostView(post: post[index]);
+                              },
+                              separatorBuilder: (context, index) =>
+                                  const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                child: Divider(),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    SliverToBoxAdapter(
-                      child: ListView.separated(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        shrinkWrap: true,
-                        primary: false,
-                        itemCount: post.length,
-                        itemBuilder: (context, index) {
-                          return PostView(post: post[index]);
-                        },
-                        separatorBuilder: (context, index) => const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          child: Divider(),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
               ),
             );
           },
