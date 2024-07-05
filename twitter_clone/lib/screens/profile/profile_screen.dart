04c0473f75ccaa4d89567dd037ac81852a987d2e
WonHyun <coy4728@naver.com>
2024-07-05 15:30:00 +0900

    fix: add await user info to profile screen

--- twitter_clone/lib/screens/profile/profile_screen.dart
+++ twitter_clone/lib/screens/profile/profile_screen.dart
@@ -9,7 +9,7 @@ import 'package:twitter_clone/screens/profile/layouts/profile_post_list.dart';
 import 'package:twitter_clone/screens/profile/layouts/user_profile_header.dart';
 import 'package:twitter_clone/screens/setting/setting_screen.dart';
 
-class ProfileScreen extends StatefulWidget {
+class ProfileScreen extends ConsumerStatefulWidget {
   const ProfileScreen({
     super.key,
     this.initTab = ProfileTab.threads,
@@ -21,10 +21,10 @@ class ProfileScreen extends StatefulWidget {
   final ProfileTab initTab;
 
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
+  ConsumerState<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen>
+class _ProfileScreenState extends ConsumerState<ProfileScreen>
     with SingleTickerProviderStateMixin {
   late final TabController _tabController = TabController(
     length: 2,
@@ -45,91 +45,98 @@ class _ProfileScreenState extends State<ProfileScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Consumer(
-        builder: (context, ref, child) {
-          final userState = ref.watch(userProvider);
-          return NestedScrollView(
-            physics: const BouncingScrollPhysics(),
-            headerSliverBuilder: (context, innerBoxIsScrolled) {
-              return [
-                SliverAppBar(
-                  surfaceTintColor: Colors.transparent,
-                  pinned: true,
-                  leading: const Center(
-                    child: FaIcon(FontAwesomeIcons.globe),
-                  ),
-                  actions: [
-                    GestureDetector(
-                      onTap: () => {},
-                      child: const FaIcon(
-                        FontAwesomeIcons.instagram,
-                        size: 30,
+    return ref.watch(userProvider).when(
+          data: (user) {
+            return Material(
+              child: NestedScrollView(
+                physics: const BouncingScrollPhysics(),
+                headerSliverBuilder: (context, innerBoxIsScrolled) {
+                  return [
+                    SliverAppBar(
+                      surfaceTintColor: Colors.transparent,
+                      pinned: true,
+                      leading: const Center(
+                        child: FaIcon(FontAwesomeIcons.globe),
                       ),
-                    ),
-                    const SizedBox(width: 20),
-                    GestureDetector(
-                      onTap: () => context.push(SettingScreen.routePath),
-                      child: const FaIcon(
-                        FontAwesomeIcons.gripLines,
-                        size: 30,
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                  ],
-                ),
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 10,
-                    ),
-                    child: UserProfileHeader(user: userState.value!),
-                  ),
-                ),
-                SliverPersistentHeader(
-                  pinned: true,
-                  delegate: PersistentTabBarHeader(
-                    backgroundColor: Theme.of(context).colorScheme.surface,
-                    child: TabBar(
-                      controller: _tabController,
-                      labelPadding: const EdgeInsets.symmetric(vertical: 10),
-                      labelColor: Theme.of(context).colorScheme.inverseSurface,
-                      labelStyle: const TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 16,
-                      ),
-                      unselectedLabelColor: Colors.grey.shade500,
-                      dividerColor: Colors.grey.shade300,
-                      indicatorColor:
-                          Theme.of(context).colorScheme.inverseSurface,
-                      indicatorSize: TabBarIndicatorSize.tab,
-                      indicatorWeight: 1.0,
-                      tabs: const [
-                        Text("Threads"),
-                        Text("Replies"),
+                      actions: [
+                        GestureDetector(
+                          onTap: () => {},
+                          child: const FaIcon(
+                            FontAwesomeIcons.instagram,
+                            size: 30,
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        GestureDetector(
+                          onTap: () => context.push(SettingScreen.routePath),
+                          child: const FaIcon(
+                            FontAwesomeIcons.gripLines,
+                            size: 30,
+                          ),
+                        ),
+                        const SizedBox(width: 20),
                       ],
                     ),
-                  ),
+                    SliverToBoxAdapter(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        child: UserProfileHeader(user: user),
+                      ),
+                    ),
+                    SliverPersistentHeader(
+                      pinned: true,
+                      delegate: PersistentTabBarHeader(
+                        backgroundColor: Theme.of(context).colorScheme.surface,
+                        child: TabBar(
+                          controller: _tabController,
+                          labelPadding:
+                              const EdgeInsets.symmetric(vertical: 10),
+                          labelColor:
+                              Theme.of(context).colorScheme.inverseSurface,
+                          labelStyle: const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                          ),
+                          unselectedLabelColor: Colors.grey.shade500,
+                          dividerColor: Colors.grey.shade300,
+                          indicatorColor:
+                              Theme.of(context).colorScheme.inverseSurface,
+                          indicatorSize: TabBarIndicatorSize.tab,
+                          indicatorWeight: 1.0,
+                          tabs: const [
+                            Text("Threads"),
+                            Text("Replies"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ];
+                },
+                body: TabBarView(
+                  controller: _tabController,
+                  children: const [
+                    ProfilePostList(
+                      key: PageStorageKey(ProfileTab.threads),
+                      tabType: ProfileTab.threads,
+                    ),
+                    ProfilePostList(
+                      key: PageStorageKey(ProfileTab.replies),
+                      tabType: ProfileTab.replies,
+                    ),
+                  ],
                 ),
-              ];
-            },
-            body: TabBarView(
-              controller: _tabController,
-              children: const [
-                ProfilePostList(
-                  key: PageStorageKey(ProfileTab.threads),
-                  tabType: ProfileTab.threads,
-                ),
-                ProfilePostList(
-                  key: PageStorageKey(ProfileTab.replies),
-                  tabType: ProfileTab.replies,
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
+              ),
+            );
+          },
+          error: (err, stack) => Center(
+            child: Text("error: $err"),
+          ),
+          loading: () => const Center(
+            child: CircularProgressIndicator.adaptive(),
+          ),
+        );
   }
 }
