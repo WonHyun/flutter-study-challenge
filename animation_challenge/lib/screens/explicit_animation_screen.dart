4aef3a4ed532e46d55531f04e41cbec0a27fb50f
WonHyun Choi <coy4728@naver.com>
2024-07-18 23:12:23 +0900

    feat: implement explicit animation (not completed)

--- animation_challenge/lib/screens/explicit_animation_screen.dart
+++ animation_challenge/lib/screens/explicit_animation_screen.dart
@@ -8,7 +8,59 @@ class ExplicitAnimationScreen extends StatefulWidget {
       _ExplicitAnimationScreenState();
 }
 
-class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen> {
+class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen>
+    with SingleTickerProviderStateMixin {
+  final rowCount = 5;
+  final columnCount = 5;
+  late final totalItems = rowCount * columnCount;
+
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 5),
+  )..repeat();
+
+  late final _tweens = List.generate(totalItems, (index) {
+    int y = index ~/ columnCount;
+    int x = index % columnCount;
+
+    int zigzagX = y.isEven ? x : columnCount - 1 - x;
+    int position = y * columnCount + zigzagX;
+
+    double start = (totalItems - 1 - position) / totalItems;
+    double end = start + (1.0 / totalItems);
+
+    return TweenSequence([
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 0.1, end: 1.0),
+        weight: 50,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 1.0, end: 0.1),
+        weight: 50,
+      ),
+    ]).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(
+          start,
+          end,
+          curve: Curves.bounceOut,
+        ),
+      ),
+    );
+  });
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +70,34 @@ class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen> {
         foregroundColor: Colors.white,
         title: const Text("Explicit Animation"),
       ),
-      body: const Column(
-        children: [],
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: columnCount,
+                mainAxisSpacing: 30,
+                crossAxisSpacing: 30,
+              ),
+              itemCount: rowCount * columnCount,
+              itemBuilder: (context, index) {
+                return AnimatedBuilder(
+                    animation: _tweens[index],
+                    builder: (context, child) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.pink.withOpacity(_tweens[index].value),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      );
+                    });
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
